1ebe2576369fdaea9c93db4ea5e12c569ea76bce
Ewan-Duplessis-Kergomard <148007352+Ewan-DuplessisK@users.noreply.github.com>
2024-04-07 17:09:41 +0200

    Handle clicks and repeated searches

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -4,6 +4,9 @@
 #include <array>
 #include <string>
 #include "raylib.h"
+#include <ctime>
+
+#include "raymath.h"
 
 enum TerrainType{
     NORMAL,
@@ -11,6 +14,13 @@ enum TerrainType{
     DIFFICULT,
     OBSTACLE
 };
+
+TerrainType next(TerrainType t){
+    if(t==NORMAL)return CHALLENGING;
+    if(t==CHALLENGING)return DIFFICULT;
+    if(t==DIFFICULT)return OBSTACLE;
+    if(t==OBSTACLE)return NORMAL;
+}
 
 struct DijkstraAnswer{
     std::vector<int> distance;
@@ -22,10 +32,14 @@ struct AStarNode{
     float hCost=0;
     float fCost=0;
     AStarNode* parent=nullptr;
-    std::array<int,2> pos;
+    Vector2 pos;
     TerrainType terrain=NORMAL;
     bool isPath = false;
 };
+
+std::vector<std::vector<AStarNode>> graph;
+Vector2 start{0,0};
+Vector2 end{0,0};
 
 DijkstraAnswer solveDijkstra(const int origin, const std::vector<std::vector<int>>* graph){
     std::vector<int> distance;
@@ -62,11 +76,24 @@ DijkstraAnswer solveDijkstra(const int origin, const std::vector<std::vector<int
     return DijkstraAnswer{distance,previous};
 }
 
-bool aStarAnswer(const std::array<int,2>origin,const std::array<int,2>goal,std::vector<std::vector<AStarNode>>* graph){
+void resetCosts(){
+    for(int i=0;i<graph.size();i++){
+        for(int j=0;j<graph.at(0).size();j++){
+            AStarNode* node = &graph.at(i).at(j);
+            node->fCost=0;
+            node->hCost=0;
+            node->gCost=0;
+            node->parent=nullptr;
+        }
+    }
+}
+
+bool aStarAnswer(const Vector2 origin,const Vector2 goal,std::vector<std::vector<AStarNode>>* graph){
+    resetCosts();
     std::vector<AStarNode*> openList;
     std::vector<AStarNode*> closedList;
-    graph->at(origin.at(0)).at(origin.at(1)).fCost=0;
-    openList.push_back(&graph->at(origin.at(0)).at(origin.at(1)));
+    graph->at(origin.x).at(origin.y).fCost=0;
+    openList.push_back(&graph->at(origin.x).at(origin.y));
     int count=0;
     while (!openList.empty()){
         AStarNode* currNode=openList.at(0);
@@ -78,49 +105,46 @@ bool aStarAnswer(const std::array<int,2>origin,const std::array<int,2>goal,std::
         closedList.push_back(currNode);
         openList.erase(std::remove(openList.begin(), openList.end(), currNode), openList.end());
 
-        if(currNode->pos==goal){
-            graph->at(goal.at(0)).at(goal.at(1)).isPath=true;
-            //std::string path = "["+std::to_string(currNode->pos.at(0))+";"+std::to_string(currNode->pos.at(1))+"]";
-            std::vector<std::array<int,2>> path;
+        if(Vector2Equals(currNode->pos,goal)){
+            graph->at(goal.x).at(goal.y).isPath=true;
+            std::vector<Vector2> path;
             AStarNode* step = currNode;
             do{
                 step = step->parent;
                 step->isPath=true;
-                //path = "["+std::to_string(step->pos.at(0))+";"+std::to_string(step->pos.at(1))+"] - "+path;
             }while(step->parent!=nullptr);
-            //path+=" "+std::to_string(currNode->gCost);
             return true;
         }else{
             for (int i = -1; i < 2; ++i){
                 for (int j = -1; j < 2; ++j){
                     if(i!=0||j!=0){
-                        std::array<int,2> childPos{currNode->pos.at(0)+i,currNode->pos.at(1)+j};
-                        if(childPos.at(0)>=0 && childPos.at(0)<graph->size() && childPos.at(1)>=0 && childPos.at(1)<graph->at(0).size()){// if child in graph
+                        Vector2 childPos{currNode->pos.x+i,currNode->pos.y+j};
+                        if(childPos.x>=0 && childPos.x<graph->size() && childPos.y>=0 && childPos.y<graph->at(0).size()){// if child in graph
                             AStarNode* child = new AStarNode;
                             float tmpGCost;
                             float tmpHCost;
                             float tmpFCost;
                             child->parent = currNode;
                             child->pos=childPos;
-                            child->terrain=graph->at(childPos.at(0)).at(childPos.at(1)).terrain;
-                            if(std::find(closedList.begin(),closedList.end(),&graph->at(childPos.at(0)).at(childPos.at(1)))==closedList.end()&& graph->at(childPos.at(0)).at(childPos.at(1)).terrain!=OBSTACLE){// if actual child not in closedList and not an obstacle
+                            child->terrain=graph->at(childPos.x).at(childPos.y).terrain;
+                            if(std::find(closedList.begin(),closedList.end(),&graph->at(childPos.x).at(childPos.y))==closedList.end()&& graph->at(childPos.x).at(childPos.y).terrain!=OBSTACLE){// if actual child not in closedList and not an obstacle
                                 tmpGCost=currNode->gCost + ((i==0||j==0?10:14)*((child->terrain==NORMAL)?1:(child->terrain==CHALLENGING)?1.5f:2.0f));
-                                const AStarNode* goalNode =&graph->at(goal.at(0)).at(goal.at(1));
-                                tmpHCost = (abs(goalNode->pos.at(0)-childPos.at(0))+abs(goalNode->pos.at(1)-childPos.at(1)))*10;
+                                const AStarNode* goalNode =&graph->at(goal.x).at(goal.y);
+                                tmpHCost = (abs(goalNode->pos.x-childPos.x)+abs(goalNode->pos.y-childPos.y))*10;
                                 tmpFCost = tmpGCost+tmpHCost;
-                                if(std::find(openList.begin(),openList.end(),&graph->at(childPos.at(0)).at(childPos.at(1)))!=openList.end()){// if actual child is in openList
-                                    if(graph->at(childPos.at(0)).at(childPos.at(1)).gCost>tmpGCost){// if recalculated cost is lower
-                                        graph->at(childPos.at(0)).at(childPos.at(1)).parent=currNode;
-                                        graph->at(childPos.at(0)).at(childPos.at(1)).gCost=tmpGCost;
-                                        graph->at(childPos.at(0)).at(childPos.at(1)).hCost=tmpHCost;
-                                        graph->at(childPos.at(0)).at(childPos.at(1)).fCost=tmpFCost;
+                                if(std::find(openList.begin(),openList.end(),&graph->at(childPos.x).at(childPos.y))!=openList.end()){// if actual child is in openList
+                                    if(graph->at(childPos.x).at(childPos.y).gCost>tmpGCost){// if recalculated cost is lower
+                                        graph->at(childPos.x).at(childPos.y).parent=currNode;
+                                        graph->at(childPos.x).at(childPos.y).gCost=tmpGCost;
+                                        graph->at(childPos.x).at(childPos.y).hCost=tmpHCost;
+                                        graph->at(childPos.x).at(childPos.y).fCost=tmpFCost;
                                     }
                                 }else{// if not in openList //the calculated cost is used.
-                                    graph->at(childPos.at(0)).at(childPos.at(1)).parent=currNode;
-                                    graph->at(childPos.at(0)).at(childPos.at(1)).gCost=tmpGCost;
-                                    graph->at(childPos.at(0)).at(childPos.at(1)).hCost=tmpHCost;
-                                    graph->at(childPos.at(0)).at(childPos.at(1)).fCost=tmpFCost;
-                                    openList.push_back(&graph->at(childPos.at(0)).at(childPos.at(1)));
+                                    graph->at(childPos.x).at(childPos.y).parent=currNode;
+                                    graph->at(childPos.x).at(childPos.y).gCost=tmpGCost;
+                                    graph->at(childPos.x).at(childPos.y).hCost=tmpHCost;
+                                    graph->at(childPos.x).at(childPos.y).fCost=tmpFCost;
+                                    openList.push_back(&graph->at(childPos.x).at(childPos.y));
                                 }
                             }
                             
@@ -132,6 +156,77 @@ bool aStarAnswer(const std::array<int,2>origin,const std::array<int,2>goal,std::
     }
     return false;
     
+}
+
+std::vector<std::vector<AStarNode>> generateGraph(int sizeX,int sizeY){
+    SetRandomSeed(std::time(nullptr));
+    std::vector<std::vector<AStarNode>> graph;
+    for(int i=0;i<sizeX;i++){
+        std::vector<AStarNode> tmp;
+        for(int j=0;j<sizeY;j++){
+            AStarNode node;
+            node.pos = Vector2{float(i),float(j)};
+            if(const int randType = GetRandomValue(0,11); randType<4){
+                node.terrain=NORMAL;
+            }else if(randType<7){
+                node.terrain=CHALLENGING;
+            }else if(randType<9){
+                node.terrain=DIFFICULT;
+            }else node.terrain=OBSTACLE;
+            if(i==0&&j==0)node.terrain=NORMAL;
+            tmp.push_back(node);
+        }
+        graph.push_back(tmp);
+    } 
+return graph;
+}
+
+void drawGraph(std::vector<std::vector<AStarNode>> graph,Vector2 start,Vector2 end){
+    for (int i = 0; i < graph.size(); ++i){
+        for (int j = 0; j < graph.at(0).size(); ++j){
+            Rectangle rec;
+            rec.height=20.0f;
+            rec.width=20.0f;
+            rec.x=i*20.0f;
+            rec.y=j*20.0f;
+            
+            switch (graph.at(i).at(j).terrain){
+                case NORMAL:DrawRectangleRec(rec,GREEN);break;
+                case CHALLENGING:DrawRectangleRec(rec,ORANGE);break;
+                case DIFFICULT:DrawRectangleRec(rec,RED);break;
+                case OBSTACLE:DrawRectangleRec(rec,BLACK);break;
+            }
+            if(graph.at(i).at(j).isPath)
+                DrawCircle(i*20.0f+10.0f,j*20.0f+10.0f,5.0f,BLACK);
+            DrawCircle(start.x*20.0f+10.0f,start.y*20.0f+10.0f,5.0f,DARKGREEN);
+            if(!Vector2Equals(end,Vector2{0,0}))DrawCircle(end.x*20.0f+10.0f,end.y*20.0f+10.0f,5.0f,BLUE);
+        }
+    }
+}
+
+void handleLeftClick(){
+    for(auto line:graph){//greedy clicked node check
+        for(auto node:line){
+            if(CheckCollisionPointRec(GetMousePosition(),Rectangle{node.pos.x*20.0f,node.pos.y*20.0f,19.5f,19.5f})){//19.5 instead of 20 insure no overlap at 2.5% size cost
+                start=Vector2{end};
+                end=Vector2{node.pos};
+            }
+        }
+    }
+    aStarAnswer(start,end,&graph);
+    drawGraph(graph,start,end);
+}
+
+void handleRightClick(){
+    for(int i=0;i<graph.size();i++){
+        for(int j=0;j<graph.at(0).size();j++){
+            AStarNode* node = &graph.at(i).at(j);
+            if(CheckCollisionPointRec(GetMousePosition(),Rectangle{node->pos.x*20.0f,node->pos.y*20.0f,19.5f,19.5f})){
+                node->terrain=next(node->terrain);
+            }
+        }
+    }
+    drawGraph(graph,start,end);
 }
 
 int main(int argc, char* argv[]){
@@ -148,43 +243,16 @@ int main(int argc, char* argv[]){
         std::vector<int>{INT_MAX, INT_MAX, 63, INT_MAX, 35, INT_MAX, 0}
     };
     DijkstraAnswer ans = solveDijkstra(2,&graph);*/
-    std::vector<std::vector<AStarNode>> graph;
-    for (int i = 0; i < 5; ++i){
-        std::vector<AStarNode> tmp;
-        for (int j = 0; j < 5; ++j){
-            AStarNode node;
-            node.pos = std::array<int,2>{i,j};
-            if(i==1)node.terrain=CHALLENGING;
-            if(i==3&&j==3)node.terrain=OBSTACLE;
-            tmp.push_back(node);
-        }
-        graph.push_back(tmp);
-    }
+    graph=generateGraph(25,25);
+    drawGraph(graph,start,end);
     while (!WindowShouldClose()){
         BeginDrawing();
-        if(aStarAnswer(std::array<int,2>{0,0},std::array<int,2>{4,4},&graph)){
-            for (int i = 0; i < graph.size(); ++i){
-                for (int j = 0; j < graph.at(0).size(); ++j){
-                    std::cout << graph.at(i).at(j).isPath<<std::endl;
-                    Rectangle rec;
-                    rec.height=100.0f;
-                    rec.width=100.0f;
-                    rec.x=i*100.0f;
-                    rec.y=j*100.0f;
-                    if(graph.at(i).at(j).isPath){
-                        DrawRectangleRec(rec,BLACK);
-                    }else{
-                        switch (graph.at(i).at(j).terrain){
-                        case NORMAL:DrawRectangleRec(rec,GREEN);break;
-                        case CHALLENGING:DrawRectangleRec(rec,BLUE);break;
-                        case DIFFICULT:DrawRectangleRec(rec,BROWN);break;
-                        case OBSTACLE:DrawRectangleRec(rec,RED);break;
-                        }
-                    }
-                }
-            }
-        }
-        std::cout << "break"<<std::endl<<std::endl;
+        std::cout << IsMouseButtonDown(MOUSE_BUTTON_LEFT)<<std::endl;
+        if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+            handleLeftClick();
+        }else if(IsMouseButtonReleased(MOUSE_BUTTON_RIGHT))handleRightClick(); //TODO change terrain difficulty on rightclick
+        else drawGraph(graph,start,end);
+        
         EndDrawing();
     }
     
